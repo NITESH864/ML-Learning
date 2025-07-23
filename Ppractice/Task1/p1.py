f61b024a4bcf184ec26824a23b29b644ab15e762
Nitesh Gupta <128721075+NITESH864@users.noreply.github.com>
2025-07-24 00:03:59 +0530

    Delete Ppractice/Task1 directory

--- Ppractice/Task1/p1.py
+++
@@ -1,3 +0,0 @@
-# Message on screen
-
-print("Hello World")
--- Ppractice/Task1/p8.py
+++
@@ -1,9 +0,0 @@
-#area ,perimeter of rectangle
-
-l=eval(input("Enter the length of rectangle:"))
-b=eval(input("Entert the breath of rectangle:"))
-
-pi=3.14
-
-print("Area of  rectangle:",l*b)
-print("Perimeter of rectangle:",2*(l+b)) 
